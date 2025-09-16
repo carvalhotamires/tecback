22aace4892575ea964600e0a86ba816f91d131bf
Tamires Carvalho <tamirescs00@gmail.com>
2025-09-16 19:41:12 -0300

    alteração no UserRepository, adicionando o import que faltava.

--- src/main/java/br/uniesp/si/techback/repository/UserRepository.java
+++ src/main/java/br/uniesp/si/techback/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package br.uniesp.si.techback.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+import br.uniesp.si.techback.model.User;
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
